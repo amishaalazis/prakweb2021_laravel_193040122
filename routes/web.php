1f10d0780696408bfea77d4babac107557af9c6f
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2021-10-15 10:19:43 +0700

    menyelesaikan video Belajar Laravel 5. Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,67 +23,17 @@ Route::get('/', function () {
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
-        "name" => "Amisha Al azis",
-        "email" => "amishaalazis@yahoo.com",
-        "image" => "ams.jpg"
+        'title' => 'About',
+        'name' => 'Amisha Al azis',
+        'email' => 'amishaalazis@yahoo.com',
+        'image' => 'ams.jpg'
     ]);
 });
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" =>"Judul Post Pertama",
-            "slug" =>"judul-post-pertama",
-            "author" => "Amisha Al azis",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero necessitatibus facilis officiis omnis est eius placeat ea pariatur neque? Beatae totam doloribus fugit ipsum facere iste omnis numquam quod!
-            "
-        ],
-        [
-            "title" =>"Judul Post Kedua",
-            "slug" =>"judul-post-kedua",
-            "author" => "Riki Blabla",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit similique adipisci blanditiis hic provident sapiente accusamus tempora incidunt laborum quaerat soluta recusandae, voluptatum voluptate, asperiores voluptates maiores consectetur aut quidem, vel ab excepturi sequi consequatur ratione. Deleniti maiores possimus, quaerat, repellendus facilis animi omnis sit architecto modi magni dolorum eum quos officia perferendis! Neque alias odio repellendus, maiores omnis aut minus nostrum accusantium sunt perspiciatis sapiente nam totam accusamus necessitatibus eaque similique sint, quod eos quia? Quae perferendis animi molestiae obcaecati optio velit excepturi esse minima, recusandae expedita fugiat tenetur dolorum ea consequatur laudantium sed sunt maxime, omnis quod corrupti! A odio, eligendi voluptatum consequatur vel obcaecati! Neque tenetur minima similique sunt accusantium voluptate optio, sed temporibus debitis. Molestiae libero perferendis voluptates non temporibus? Sit deleniti optio asperiores, quibusdam soluta, maxime dolorum possimus modi minima vero, commodi nulla. Itaque neque cupiditate officiis minima quod odio perspiciatis dolores mollitia. Dolore suscipit nobis nisi fugit aperiam nihil sint sed dolorum est eaque sunt explicabo reprehenderit deserunt saepe unde, corporis reiciendis beatae aut similique repellat praesentium quibusdam. Doloremque fugiat quae fuga, sapiente vero accusantium, laboriosam officia eveniet voluptatem rem natus velit quia beatae? Vel dolorem culpa consectetur? Quia quae quo voluptatibus laborum voluptas!"
-        ],
-        
-        ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 
 //halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" =>"Judul Post Pertama",
-            "slug" =>"judul-post-pertama",
-            "author" => "Amisha Al azis",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero necessitatibus facilis officiis omnis est eius placeat ea pariatur neque? Beatae totam doloribus fugit ipsum facere iste omnis numquam quod!
-            "
-        ],
-        [
-            "title" =>"Judul Post Kedua",
-            "slug" =>"judul-post-kedua",
-            "author" => "Riki Blabla",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit similique adipisci blanditiis hic provident sapiente accusamus tempora incidunt laborum quaerat soluta recusandae, voluptatum voluptate, asperiores voluptates maiores consectetur aut quidem, vel ab excepturi sequi consequatur ratione. Deleniti maiores possimus, quaerat, repellendus facilis animi omnis sit architecto modi magni dolorum eum quos officia perferendis! Neque alias odio repellendus, maiores omnis aut minus nostrum accusantium sunt perspiciatis sapiente nam totam accusamus necessitatibus eaque similique sint, quod eos quia? Quae perferendis animi molestiae obcaecati optio velit excepturi esse minima, recusandae expedita fugiat tenetur dolorum ea consequatur laudantium sed sunt maxime, omnis quod corrupti! A odio, eligendi voluptatum consequatur vel obcaecati! Neque tenetur minima similique sunt accusantium voluptate optio, sed temporibus debitis. Molestiae libero perferendis voluptates non temporibus? Sit deleniti optio asperiores, quibusdam soluta, maxime dolorum possimus modi minima vero, commodi nulla. Itaque neque cupiditate officiis minima quod odio perspiciatis dolores mollitia. Dolore suscipit nobis nisi fugit aperiam nihil sint sed dolorum est eaque sunt explicabo reprehenderit deserunt saepe unde, corporis reiciendis beatae aut similique repellat praesentium quibusdam. Doloremque fugiat quae fuga, sapiente vero accusantium, laboriosam officia eveniet voluptatem rem natus velit quia beatae? Vel dolorem culpa consectetur? Quia quae quo voluptatibus laborum voluptas!"
-        ],
-        
-        ];
-
-        $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-
-        }
-    }
-    return view('post', [
-        "title" =>"Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);

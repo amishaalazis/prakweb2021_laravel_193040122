a79ede3d67b68ff1cccbbccb0522300d93ee3d03
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2021-10-15 15:58:18 +0700

    menyelesaikan video Belajar Laravel 8.Post Category & Eloquent

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-
 <h1 class = "mb-5">{{ $post->title }}</h1>
+<p>By. Amisha Al azis in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
+
 {!! $post->body !!}
 
 

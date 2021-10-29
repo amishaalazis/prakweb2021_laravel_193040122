fde10f8157e3dc6cadfc982f5746857f6d3e9e20
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2021-10-29 13:38:34 +0700

    menyelesaikan video Belajar Laravel 15. User Registration

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,8 @@ Route::get('/login', [LoginController::class, 'index']);
 
 Route::get('/register', [RegisterController::class, 'index']);
 
+Route::post('/register', [RegisterController::class, 'store']);
+
 
 
 
